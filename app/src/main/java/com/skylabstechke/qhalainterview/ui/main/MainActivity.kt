c6309ecb97e6271ae9a95ebc64f9b38bcde93e15
EricRootLee <erickiokom@gmail.com>
2021-06-12 15:30:39 +0300

    Test

--- app/src/main/java/com/skylabstechke/qhalainterview/ui/main/MainActivity.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/ui/main/MainActivity.kt
@@ -1,7 +1,9 @@
 package com.skylabstechke.qhalainterview.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 import com.skylabstechke.qhalainterview.R
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -10,5 +12,11 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setupActionBarWithNavController(findNavController(R.id.navHostContainer))
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        val navHost = findNavController(R.id.navHostContainer).navigateUp()
+        return navHost || super.onSupportNavigateUp()
     }
 }
