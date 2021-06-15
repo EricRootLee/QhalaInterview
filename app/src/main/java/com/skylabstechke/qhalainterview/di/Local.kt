a0e58a1a739533dfa1e89e94f02197a4d5e9b05f
EricRootLee <erickiokom@gmail.com>
2021-06-15 23:05:03 +0300

    Provides database

--- app/src/main/java/com/skylabstechke/qhalainterview/di/Local.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/di/Local.kt
@@ -1,11 +1,33 @@
 package com.skylabstechke.qhalainterview.di
 
+import android.content.Context
+import androidx.room.Room
+import com.skylabstechke.qhalainterview.data.local.MoviesDatabase
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.components.ApplicationComponent
+import dagger.hilt.android.qualifiers.ApplicationContext
+import javax.inject.Singleton
 
 
 @InstallIn(ApplicationComponent::class)
 @Module
 object Local {
+
+    @Singleton
+    @Provides
+
+    fun provideDatabase(@ApplicationContext context: Context) = Room.databaseBuilder(
+        context,
+        MoviesDatabase::class.java,
+        "movies_database"
+    ).build()
+
+
+    @Singleton
+    @Provides
+    fun provideMoviesDao(database: MoviesDatabase) = database.moviesDao()
+
+
 }
