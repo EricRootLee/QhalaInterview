6aecce5cf54a9218dc9194e2a8a00e16659fe56d
EricRootLee <erickiokom@gmail.com>
2021-06-15 19:12:40 +0300

    Smaple test

--- app/src/main/java/com/skylabstechke/qhalainterview/adapters/RecyclerViewAdapter.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/adapters/RecyclerViewAdapter.kt
@@ -2,13 +2,14 @@ package com.skylabstechke.qhalainterview.adapters
 
 import android.view.LayoutInflater
 import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.RecyclerView
 import com.skylabstechke.qhalainterview.databinding.PopularMoviesRowLayoutBinding
 import com.skylabstechke.qhalainterview.utils.DiffUtils
 
 
 class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolder>() {
-    private val dataList = emptyList<String>()
+    private var dataList = listOf<String>("Muli", "denno", "cherono", "mbuta", "kanga", "uweri")
 
     inner class MyViewHolder(val binding: PopularMoviesRowLayoutBinding) :
         RecyclerView.ViewHolder(binding.root)
@@ -24,15 +25,21 @@ class RecyclerViewAdapter : RecyclerView.Adapter<RecyclerViewAdapter.MyViewHolde
     }
 
     override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
-        holder.binding.movieTitle.text = "Title"
+        holder.binding.movieTitle.text = dataList[position]
     }
 
     override fun getItemCount(): Int {
         return dataList.size
     }
 
-    fun setData(newData: List<Int>) {
-        val diffUtil = DiffUtils()
+    fun setData(newData: List<String>) {
+        val diffUtil = DiffUtils(
+            dataList,
+            newData
+        )
+        val diffUtilResults = DiffUtil.calculateDiff(diffUtil)
+        diffUtilResults.dispatchUpdatesTo(this)
+        dataList = newData
 
     }
 }
