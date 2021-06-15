1e13fe030e0ebdf77a47d63bfc24ffd28e42d721
EricRootLee <erickiokom@gmail.com>
2021-06-15 22:14:07 +0300

    Interface

--- app/src/main/java/com/skylabstechke/qhalainterview/data/network/MoviesApi.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/data/network/MoviesApi.kt
@@ -1,4 +1,11 @@
 package com.skylabstechke.qhalainterview.data.network
 
+import kotlinx.coroutines.flow.Flow
+import retrofit2.Response
+import retrofit2.http.GET
+
 interface MoviesApi {
+    @GET("/movies")
+    suspend fun getMovies(
+    ): Flow<Response<Int>>
 }
