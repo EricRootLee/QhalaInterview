941f4e50d50755ac2fded522cd450a60d184d486
EricRootLee <erickiokom@gmail.com>
2021-06-15 19:30:17 +0300

    Testing

--- app/src/main/java/com/skylabstechke/qhalainterview/ui/fragments/PopularMoviesFragment.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/ui/fragments/PopularMoviesFragment.kt
@@ -5,15 +5,46 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import com.skylabstechke.qhalainterview.R
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.skylabstechke.qhalainterview.adapters.RecyclerViewAdapter
+import com.skylabstechke.qhalainterview.databinding.FragmentPopularMoviesBinding
 
 
 class PopularMoviesFragment : Fragment() {
+
+    private var _binding: FragmentPopularMoviesBinding? = null
+    private val binding get() = _binding!!
+
+    private val recyclerViewAdapter by lazy { RecyclerViewAdapter() }
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_popular_movies, container, false)
+        _binding = FragmentPopularMoviesBinding.inflate(
+            layoutInflater,
+            container,
+            false
+        )
+        setRecyclerView()
+        showShimmer()
+        return binding.root
+    }
+
+    private fun setRecyclerView() {
+        binding.recyclerViewId.adapter = recyclerViewAdapter
+        binding.recyclerViewId.layoutManager = LinearLayoutManager(requireContext())
+    }
+
+    private fun showShimmer() {
+        binding.recyclerViewId.showShimmer()
+    }
+
+    private fun hideShimmer() {
+        binding.recyclerViewId.hideShimmer()
+    }
+
+    private fun requestApi(){
+        showShimmer()
     }
 }
