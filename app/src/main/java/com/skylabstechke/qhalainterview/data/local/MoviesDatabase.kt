baa1540e242320dcb0d7539a753f50fb45b1b1ed
EricRootLee <erickiokom@gmail.com>
2021-06-15 22:59:35 +0300

    ...

--- app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesDatabase.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesDatabase.kt
@@ -1,7 +1,17 @@
 package com.skylabstechke.qhalainterview.data.local
 
+import androidx.room.Database
 import androidx.room.RoomDatabase
+import androidx.room.TypeConverter
+import androidx.room.TypeConverters
 
+@Database(
+    entities = [MoviesEntity::class],
+    version = 1,
+    exportSchema = false
+)
+
+@TypeConverters(MoviesTypeConvertor::class)
 abstract class MoviesDatabase :RoomDatabase() {
     abstract fun moviesDao():MoviesDao
 }
