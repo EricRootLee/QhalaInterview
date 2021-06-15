4ef9293371c809d6073d825fa0e9d75ef58d529a
EricRootLee <erickiokom@gmail.com>
2021-06-16 01:09:14 +0300

    net

--- app/src/main/java/com/skylabstechke/qhalainterview/application/QhalaInterview.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/application/QhalaInterview.kt
@@ -1,7 +1,22 @@
 package com.skylabstechke.qhalainterview.application
 
 import android.app.Application
+import android.os.Build
+import androidx.annotation.RequiresApi
+import com.skylabstechke.qhalainterview.utils.NetworkMonitor
 import dagger.hilt.android.HiltAndroidApp
 
 @HiltAndroidApp
-class QhalaInterview : Application()
+class QhalaInterview : Application() {
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onCreate() {
+        super.onCreate()
+        NetworkMonitor(this).startNetWorkCallback()
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onTerminate() {
+        super.onTerminate()
+        NetworkMonitor(this).stopNetworkCallback()
+    }
+}
