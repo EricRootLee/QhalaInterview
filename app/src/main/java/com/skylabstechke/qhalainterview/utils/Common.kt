9127b79fd4c12d604e1c63b7bf8aa96f52c73704
EricRootLee <erickiokom@gmail.com>
2021-06-17 04:25:30 +0300

    Minor

--- app/src/main/java/com/skylabstechke/qhalainterview/utils/Common.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/utils/Common.kt
@@ -1,10 +1,9 @@
 package com.skylabstechke.qhalainterview.utils
 
-class Common {
-    companion object {
 
-        const val BASE_URL = ""
-        const val API_KEY = ""
+object Common {
 
-    }
+    const val BASE_URL = ""
+    const val API_KEY = ""
+
 }
