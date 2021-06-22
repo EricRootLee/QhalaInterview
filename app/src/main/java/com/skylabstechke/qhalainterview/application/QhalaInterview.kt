f0fdf0dc6ac021bb68bea20e79e939b2b548c216
EricRootLee <erickiokom@gmail.com>
2021-06-22 15:31:46 +0300

    PLant timber

--- app/src/main/java/com/skylabstechke/qhalainterview/application/QhalaInterview.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/application/QhalaInterview.kt
@@ -5,13 +5,19 @@ import android.os.Build
 import androidx.annotation.RequiresApi
 import com.skylabstechke.qhalainterview.utils.NetworkMonitor
 import dagger.hilt.android.HiltAndroidApp
+import timber.log.Timber
 
 @HiltAndroidApp
 class QhalaInterview : Application() {
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
     override fun onCreate() {
         super.onCreate()
+        //start network monitor
         NetworkMonitor(this).startNetWorkCallback()
+
+          //plant timber
+         Timber.plant(Timber.DebugTree())
+        //
     }
 
     @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
