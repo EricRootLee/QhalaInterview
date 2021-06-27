669fe9253e952754d9b051c7aeeb39c89319f88a
EricRootLee <erickiokom@gmail.com>
2021-06-27 21:09:28 +0300

    Added api

--- app/src/main/java/com/skylabstechke/qhalainterview/utils/Common.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/utils/Common.kt
@@ -2,8 +2,10 @@ package com.skylabstechke.qhalainterview.utils
 
 
 object Common {
+    const val API_KEY = "36eef54edeb7815e3cc5a7497b3afcfa"
 
-    const val BASE_URL = ""
-    const val API_KEY = ""
+    const val BASE_URL =
+        "https://api.themoviedb.org/3/movie/popular?api_key=$API_KEY&language=en-US&page=1"
+
 
 }
