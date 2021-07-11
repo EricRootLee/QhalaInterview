fc5fc7e4c01f8f062b757029cb22ec6945d7ba60
EricRootLee <erickiokom@gmail.com>
2021-07-11 03:48:40 +0300

    Type convertor

--- app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesEntity.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesEntity.kt
@@ -8,7 +8,6 @@ import com.skylabstechke.qhalainterview.utils.Common.MOVIES_TABLE
 @Entity(tableName = MOVIES_TABLE)
 class MoviesEntity(
     private val moviesData: MoviesModel
-
 ) {
 
     @PrimaryKey(autoGenerate = false)
--- app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesTypeConvertor.kt
+++ app/src/main/java/com/skylabstechke/qhalainterview/data/local/MoviesTypeConvertor.kt
@@ -3,7 +3,22 @@ package com.skylabstechke.qhalainterview.data.local
 import androidx.room.TypeConverter
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
+import com.skylabstechke.qhalainterview.data.models.MoviesModel
 
 
-class MoviesTypeConvertor
+class MoviesTypeConvertor(){
+    var gson = Gson()
+
+    @TypeConverter
+    fun moviesToString(movies:MoviesModel):String{
+        return  gson.toJson(movies)
+    }
+
+    @TypeConverter
+    fun stringToMovies(data:String):MoviesModel{
+        val listType  = object  : TypeToken<MoviesModel>() {}.type
+        return gson.fromJson(data,listType  )
+    }
+
+}
 
