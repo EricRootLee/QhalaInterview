c4aacd1fda3168741e8ef4d8f25f8c1a0389ca3a
EricRootLee <erickiokom@gmail.com>
2021-07-15 10:17:43 +0300

    Test

--- app/src/test/java/com/skylabstechke/qhalainterview/ExampleUnitTest.kt
+++ app/src/test/java/com/skylabstechke/qhalainterview/ExampleUnitTest.kt
@@ -1,17 +1,23 @@
 package com.skylabstechke.qhalainterview
 
+import com.skylabstechke.qhalainterview.data.local.MoviesDao
+import com.skylabstechke.qhalainterview.data.local.MoviesDatabase
 import org.junit.Test
-
 import org.junit.Assert.*
+import org.junit.Rule
+import org.junit.rules.TestRule
+import org.junit.runner.RunWith
 
 /**
  * Example local unit test, which will execute on the development machine (host).
  *
  * See [testing documentation](http://d.android.com/tools/testing).
  */
+
 class ExampleUnitTest {
     @Test
     fun addition_isCorrect() {
         assertEquals(4, 2 + 2)
     }
+  
 }
